6c058f35747486246e11daf3ad56fe564a0a98c2
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2021-04-07 13:43:41 +0100

    Update path for building API reference

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(0, os.path.abspath('../src/diffcalc'))
+sys.path.insert(0, os.path.abspath('../src/diffcalc/hkl'))
+sys.path.insert(0, os.path.abspath('../src/diffcalc/ub'))
 
 import diffcalc
 
